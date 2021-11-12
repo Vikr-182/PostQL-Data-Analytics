d67f9c48e778c7773e8553e21659369b97c88dfd
Vikr-182 <vikrant.dewangan@research.iiit.ac.in>
2021-11-12 21:50:39 +0530

    added code

--- FPgrowth.py
+++ FPgrowth.py
@@ -12,69 +12,41 @@ import collections
 from distutils.version import LooseVersion as Version
 from pandas import __version__ as pandas_version
 
-class FPTree(object):
-    def __init__(self, rank=None):
-        self.root = FPNode(None)
+class ConditionedFPTree(object):
+    def __init__(self, ordering=None):
+        self.ordering = ordering        
+        self.root = Node(None)
         self.nodes = collections.defaultdict(list)
         self.cond_items = []
-        self.rank = rank
 
     def conditional_tree(self, cond_item, minsup):
-        """
-        Creates and returns the subtree of self conditioned on cond_item.
-
-        Parameters
-        ----------
-        cond_item : int | str
-            Item that the tree (self) will be conditioned on.
-        minsup : int
-            Minimum support threshold.
-
-        Returns
-        -------
-        cond_tree : FPtree
-        """
-        # Find all path from root node to nodes for item
         branches = []
         count = collections.defaultdict(int)
         for node in self.nodes[cond_item]:
-            branch = node.itempath_from_root()
+            branch = node.fine_path_from_root()
             branches.append(branch)
             for item in branch:
                 count[item] += node.count
 
-        # Define new ordering or deep trees may have combinatorially explosion
         items = [item for item in count if count[item] >= minsup]
         items.sort(key=count.get)
-        rank = {item: i for i, item in enumerate(items)}
+        ordering = {item: i for i, item in enumerate(items)}
 
-        # Create conditional tree
-        cond_tree = FPTree(rank)
+        cond_tree = ConditionedFPTree(ordering)
         for idx, branch in enumerate(branches):
-            branch = sorted([i for i in branch if i in rank],
-                            key=rank.get, reverse=True)
-            cond_tree.insert_itemset(branch, self.nodes[cond_item][idx].count)
+            branch = sorted([i for i in branch if i in ordering],
+                            key=ordering.get, reverse=True)
+            cond_tree.insert_transaction(branch, self.nodes[cond_item][idx].count)
         cond_tree.cond_items = self.cond_items + [cond_item]
 
         return cond_tree
 
-    def insert_itemset(self, itemset, count=1):
-        """
-        Inserts a list of items into the tree.
-
-        Parameters
-        ----------
-        itemset : list
-            Items that will be inserted into the tree.
-        count : int
-            The number of occurrences of the itemset.
-        """
+    def insert_transaction(self, itemset, count=1):
         self.root.count += count
 
         if len(itemset) == 0:
             return
 
-        # Follow existing path in tree as long as possible
         index = 0
         node = self.root
         for item in itemset:
@@ -88,7 +60,7 @@ class FPTree(object):
 
         # Insert any remaining items
         for item in itemset[index:]:
-            child_node = FPNode(item, count, node)
+            child_node = Node(item, count, node)
             self.nodes[item].append(child_node)
             node = child_node
 
@@ -108,17 +80,17 @@ class FPTree(object):
         print('\r%d itemset(s) from tree conditioned on items (%s)' %
               (count, cond_items), end="\n")
 
-class FPNode(object):
+class Node(object):
     def __init__(self, item, count=0, parent=None):
         self.item = item
         self.count = count
         self.parent = parent
-        self.children = collections.defaultdict(FPNode)
+        self.children = collections.defaultdict(Node)
 
         if parent is not None:
             parent.children[item] = self
 
-    def itempath_from_root(self):
+    def fine_path_from_root(self):
         """ Returns the top-down sequence of items from self to
             (but not including) the root node. """
         path = []
@@ -146,19 +118,19 @@ def setup_fptree(df, min_support):
 
     # Define ordering on items for inserting into FPTree
     indices = item_support[items].argsort()
-    rank = {item: i for i, item in enumerate(items[indices])}
+    ordering = {item: i for i, item in enumerate(items[indices])}
 
     # Building tree by inserting itemsets in sorted order
     # Heuristic for reducing tree size is inserting in order
     #   of most frequent to least frequent
-    tree = FPTree(rank)
+    tree = ConditionedFPTree(ordering)
     for i in range(num_itemsets):
         nonnull = np.where(itemsets[i, :])[0]
-        itemset = [item for item in nonnull if item in rank]
-        itemset.sort(key=rank.get, reverse=True)
-        tree.insert_itemset(itemset)
+        itemset = [item for item in nonnull if item in ordering]
+        itemset.sort(key=ordering.get, reverse=True)
+        tree.insert_transaction(itemset)
 
-    return tree, rank
+    return tree, ordering
 
 def generate_itemsets(generator, num_itemsets, colname_map):
     itemsets = []
@@ -192,12 +164,12 @@ def FPgrowth(df, min_support=0.5, use_colnames=False, max_len=None, verbose=0):
 
     tree, _ = setup_fptree(df, min_support)
     minsup = math.ceil(min_support * len(df.index))  # min support as count
-    generator = fpg_step(tree, minsup, colname_map, max_len, verbose)
+    generator = compute_fpg(tree, minsup, colname_map, max_len, verbose)
 
     return generate_itemsets(generator, len(df.index), colname_map)
 
 
-def fpg_step(tree, minsup, colnames, max_len, verbose):
+def compute_fpg(tree, minsup, colnames, max_len, verbose):
     count = 0
     items = tree.nodes.keys()
     if tree.is_path():
@@ -224,6 +196,6 @@ def fpg_step(tree, minsup, colnames, max_len, verbose):
     if not tree.is_path() and (not max_len or max_len > len(tree.cond_items)):
         for item in items:
             cond_tree = tree.conditional_tree(item, minsup)
-            for sup, iset in fpg_step(cond_tree, minsup,
+            for sup, iset in compute_fpg(cond_tree, minsup,
                                       colnames, max_len, verbose):
                 yield sup, iset
